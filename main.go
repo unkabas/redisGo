e3145e1ba7f208dd60cd0b07f8b6282144bca843
unkabas <undercannabis10@gmail.com>
2025-06-22 23:41:39 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/unkabas/redisGo/config"
+	"io"
+	"net/http"
+	"os"
+)
+
+type Weather struct {
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func getWeather(c *gin.Context) {
+
+	city := c.Param("city")
+	link := os.Getenv("weatherUrl") + city + "&appid=" + os.Getenv("apiKey") + "&units=metric"
+	data, err := http.Get(link)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message": "fuck",
+		})
+		return
+	}
+	body, err := io.ReadAll(data.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return
+	}
+	c.JSON(200, gin.H{
+		"message": weather.Main.Temp,
+	})
+}
+func main() {
+	config.loadEnv()
+	r := gin.Default()
+
+	r.GET("/weather/:city", getWeather)
+	r.Run(":8080")
+}
